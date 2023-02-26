aaed9fe44d53377d6184b9d06383ad59a72b5b83
kkkovalov <vlad_kovalov@icloud.com>
2023-02-25 23:52:47 -0800

    Added more functionality to the class List, such as: front, back, pop_front, pop_back, valueAt, delete

--- index.cpp
+++ index.cpp
@@ -2,6 +2,9 @@
 
 class List {
     public:
+        List(){
+            head = NULL;
+        }
         int size(){
             ListNode *temp = head;
             if(temp == NULL){
@@ -17,7 +20,10 @@ class List {
         }
 
         void push_front(int newValue){
-
+            ListNode *newNode = (struct ListNode*) malloc(sizeof(ListNode));
+            newNode->value = newValue;
+            newNode->next = head;
+            head = newNode;
         };
 
         void push_back(int newValue){
@@ -35,8 +41,68 @@ class List {
             };
         };
 
+        int pop_front(){
+            if(isEmpty()) return -1;
+            ListNode *temp = head;
+            head = head->next;
+            int deleteValue = temp->value;
+            free(temp);
+            return deleteValue;
+        };
+
+        int pop_back(){
+            if(isEmpty()) return -1;
+            ListNode *temp = head;
+            while(temp->next->next){
+                temp = temp->next;
+            }
+            int deleteValue = temp->next->value;
+            free(temp->next);
+            temp->next = NULL;
+            return deleteValue;
+        };
+
+        void insert(int index, int value){
+            if(index >= size()) {
+                std::cout<<"Index is out of range, added at the back \n";
+                push_back(value);
+            } else {
+                ListNode *newNode = (struct ListNode*) malloc(sizeof(ListNode));
+                newNode->value = value;
+                int i = 0;
+                ListNode *temp = head;
+                while( i != index - 1 ){
+                    temp = temp->next;
+                    i++;
+                };
+                newNode->next = temp->next;
+                temp->next = newNode;
+            }
+        };
+
+        int erase(int index){
+            if(index >= size()) {
+                std::cout<<"Index is out of range, removed from the back";
+                pop_back();
+            } else {
+                ListNode *temp = head;
+                int i = 0;
+                while(i != index - 1){
+                    temp = temp->next;
+                    i++;
+                }
+                ListNode *erasedNode = temp->next;
+                int erasedValue = temp->next->value;
+                if(temp->next->next != NULL) temp->next = temp->next->next;
+                free(erasedNode);
+                return erasedValue;
+            }
+            return -1;
+        };
+
         void display(){
             ListNode *temp = head;
+            std::cout<<"Head: "<<head<<std::endl;
             while(temp){
                 std::cout<<temp->value<<' '<<temp->next<<std::endl;
                 temp = temp->next;
@@ -44,12 +110,45 @@ class List {
             std::cout<<std::endl;
         };
 
+        bool isEmpty(){
+            if(head) return 0;
+            else return 1;
+        };
+
+        int valueAt(int index){
+            if(isEmpty()) return -1;
+            if(index >= size()) return -1;
+            int i = 0;
+            ListNode *temp = head;
+            while( i != index){
+                i++;
+                temp = temp->next;
+            }
+            return temp->value;
+        };
+
+        int front(){
+            if(isEmpty()) return -1;
+            else return head->value;
+        };
+        
+        int back(){
+            if(isEmpty()) return -1;
+            else {
+                ListNode *temp = head;
+                while(temp->next){
+                    temp = temp->next;
+                }
+                return temp->value;
+            }
+        };
+
     private:
         struct ListNode {
             int value;
             ListNode *next;
         };
-        ListNode *head = NULL;
+        ListNode *head;
         
 };
 
@@ -57,10 +156,20 @@ class List {
 int main(void){
 
     List list;
+    list.push_front(2);
     list.push_back(6);
+    list.push_front(3);
     list.push_back(4);
     list.push_back(1);
     list.push_back(2);
+    list.push_front(9);
+    list.insert(1,5);
     list.display();
-
+    std::cout<<"Erased value at index '4': "<<list.erase(4)<<std::endl;
+    list.display();
+    std::cout<<"Value at index '0': "<<list.valueAt(0)<<std::endl;
+    std::cout<<"isEmpty: "<<list.isEmpty()<<std::endl;
+    std::cout<<"Size: "<<list.size()<<std::endl;
+    std::cout<<"Value at the front: "<<list.front()<<std::endl;
+    std::cout<<"Value at the back: "<<list.back()<<std::endl;
 };
